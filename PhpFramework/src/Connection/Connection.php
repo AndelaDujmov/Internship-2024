17695c905e9e58fdd0ad5570a583e7ea89dcbe50
AndelaDujmov <andeladujmov3@gmail.com>
2024-07-19 22:17:10 +0200

    updated class COnnection

--- PhpFramework/src/Connection/Connection.php
+++ PhpFramework/src/Connection/Connection.php
@@ -4,15 +4,18 @@ class Connection{
 
     private $pdo;
 
-    public function __construct($dsn, $username, $password) {
+    public function __construct(string $dsn, string $username, #[SensitiveParameter] ?string $password = null) {
         $this->pdo = new PDO($dsn, $username, $password);
     }
 
     public function fetchAssoc(string $query, array $options = []) : array {
-        $this->pdo->prepare($query);
+        $pdoStatement = $this->pdo->prepare($query);
+        $pdoStatement->execute();
+        return $pdoStatement->fetch(PDO::FETCH_ASSOC);
     }
 
     public function fetchAssocAll(string $query, array $options = []) : array {
         $this->pdo->prepare($query);
     }
+
 }
