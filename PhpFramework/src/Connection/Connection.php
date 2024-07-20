1c4a04a07a53d3184166373b20e38ecf079c8581
AndelaDujmov <andeladujmov3@gmail.com>
2024-07-20 15:23:43 +0200

    added update function and insert multiple - needs fixing

--- PhpFramework/src/Connection/Connection.php
+++ PhpFramework/src/Connection/Connection.php
@@ -27,6 +27,88 @@ class Connection{
         return $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    public function insert(string $tableName, array $assocArray = []) : bool {
+        /*
+        if (isset($assocArray[0]) && is_array($assocArray[0])){
+            return $this->groupInsert($tableName, $assocArray);
+        }*/
+        return $this->singleInsert($tableName, $assocArray);
+    }
+
+    public function update(string $tableName, array $columnValues = [], array $condition = []) : bool {
+        $setPlaceholder = [];
+        $setValues = [];
+        $wherePlaceholder = [];
+        $whereValues = [];
+
+        foreach ($columnValues as $key => $value){
+            $setPlaceholder[] = "$key = :set$key";
+            $setValues[":set$key"] = $value;
+        }
+
+        foreach ($condition as $key => $value){
+            $wherePlaceholder[] = "$key = :where$key";
+            $whereValues[":where$key"] = $value;
+        }
+
+        $set = implode(', ', $setPlaceholder);
+        $where = implode(' AND ', $wherePlaceholder);
+
+        $query = 'UPDATE $tableName SET $set WHERE $where';
+
+        $pdoStatement = $this->dbh->prepare($query);
+
+        foreach ($setValues as $key => $value){
+            $pdoStatement->bindValue($key, $value);
+        }
+
+        foreach ($whereValues as $key => $value){
+            $pdoStatement->bindValue($key, $value);
+        }
+
+        return $pdoStatement->execute();
+    }
+
+    /*
+    private function groupInsert(string $tableName, array $assocArray = []) : bool{
+        if (empty($assocArray))
+            throw new InvalidArgumentException("Wrong data format for input.");
+
+        $columnArray = array_keys($assocArray);
+
+        foreach ($assocArray as $rowIndex => $row){
+
+        }
+    }
+    */
+
+    private function singleInsert(string $tableName, array $assocArray = []) : bool {
+        if (empty($assocArray))
+            throw new InvalidArgumentException("Wrong data format for input.");
+
+        $columnArray = array_keys($assocArray);
+        $placeholderArray = [];
+        $values = [];
+    
+        foreach($assocArray as $key => $value){
+            $placeholderArray[] = ":$key";
+            $values[":$key"] = $value;
+        }
+
+        $columns = implode(', ', $columnArray);
+        $placeholders = implode(', ', $placeholderArray);
+
+        $query = 'INSERT INTO $tableName ($columns) VALUES ($placeholders)';
+
+        $pdoStatement = $this->dbh->prepare($query);
+
+        foreach($values as $key => $value){
+            $pdoStatement->bindValue($key, $value);
+        }
+
+        return $pdoStatement->execute();
+    }
+
     private function execute(string $query, array $params = []) : PDOStatement {
         $pdoStatement = $this->dbh->prepare($query);
         
@@ -45,4 +127,5 @@ class Connection{
         $pdoStatement->execute($params);
         return $pdoStatement;
     }
+
 }
