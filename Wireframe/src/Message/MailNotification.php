2ef5653e12f4c4d27ea1f4be0259886b7d34f3c1
AndelaDujmov <andeladujmov3@gmail.com>
2024-08-09 14:33:29 +0200

    added messenger

--- Wireframe/src/Message/MailNotification.php
+++ Wireframe/src/Message/MailNotification.php
@@ -4,12 +4,26 @@ namespace App\Message;
 
 class MailNotification {
 
-    public function __construct(private string $content) {
-        
+    private string $to;
+    private string $subject;
+    private string $content;
+
+    public function __construct(string $to, string $subject, string $content) {
+        $this->to = $to;
+        $this->subject = $subject;
+        $this->content = $content;
+    }
+
+    public function getTo(): string {
+        return $this->to;
     }
 
     public function getContent(): string {
         return $this->content;
+    }
+
+    public function getSubject(): string {
+        return $this->subject;
     }
     
 }
--- Wireframe/src/Message/MailNotificationHandler.php
+++ Wireframe/src/Message/MailNotificationHandler.php
@@ -1,10 +1,32 @@
 <?php
 
+use App\Message\MailNotification;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Mime\Email;
 
 #[AsMessageHandler]
 class MailNotificationHandler {
-    public function __invoke() {
-        
+
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer){
+        $this->mailer = $mailer;
+    }
+
+    public function __invoke(MailNotification $mail)
+    {
+        $email = (new Email())
+        ->from("andeladujmov9@gmail.com")
+        ->to($mail->getTo())
+        ->subject($mail->getSubject())
+        ->text($mail->getContent());
+
+        try{
+            $this->mailer->send($email);
+        }catch(\Exception $e){
+            throw new \Exception($e->getMessage());
+        }
+           
     }
 }
